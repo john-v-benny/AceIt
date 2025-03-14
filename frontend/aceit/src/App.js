e484f92bcc69f3bcf99e0cf14287e4069d779078
merin pr <merinpr4@gmail.com>
2025-03-14 18:11:00 +0530

    added_loginPage

--- frontend/aceit/src/App.js
+++ frontend/aceit/src/App.js
@@ -4,8 +4,9 @@ import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Features from "./pages/Features/Features";
 import InterviewPage from "./pages/InterviewPage/InterviewPage";
+import Login from "./pages/Authentication/Login/Login";
 import SignUp from "./pages/Authentication/SignUp/SignIn";
-import "./SignUp.css";
+
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/features" element={<Features />} />
         <Route path="/InterviewPage" element={<InterviewPage />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/signUp" element={<SignUp />} />
       </Routes>
     </Router>
